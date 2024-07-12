46d618da60785618fab532950b5dd3a29db546b9
TUBE-GULL <ezapis11@gmail.com>
2024-07-12 21:09:42 +0300

    0.0.3

--- app/handlers.py
+++ app/handlers.py
@@ -1,25 +1,27 @@
 import asyncio
-from aiogram import Bot, Dispatcher,F
+from aiogram import F,Router
 from aiogram.filters import CommandStart,Command
 from aiogram.types import Message
 
+router = Router()
+
 # command START !
-@dp.message(CommandStart())
+@router.message(CommandStart())
 async def  cmd_start(message:Message):
     await message.answer('hi !')
 
 # work with PHOTO
-@dp.message(F.photo)
+@router.message(F.photo)
 async def get_photo(message:Message):
     #  тут получаю id photo  await message.answer(f"ID photo {message.photo[-1].file_id}")
     await message.answer_photo(photo = message.photo[-1].file_id , caption="I do not with photos")
 
 # command F 
-@dp.message(F.text == 'как дела?')
+@router.message(F.text == 'как дела?')
 async def how_are_you(message:Message):
     await message.answer('ok!')
 
 # command HELP !
-@dp.message(Command('help'))
+@router.message(Command('help'))
 async def get_help(message:Message):
     await message.answer("this command /help")
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from log import my_logo 
-from app import read_tokens
+from app.read_tokens import TOKEN
+from app.handlers import router
 
-bot = Bot(token = read_tokens.TOKEN[0])
+bot = Bot(token = TOKEN[0])
 dp = Dispatcher()
 
 async def main():
     my_logo.logo()
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__  == '__main__':
